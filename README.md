bf345bfdd246b5603c61ba5cd435c912932de612
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2025-07-01 21:47:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Leaf Chatbot
+
+```shell
+export SPRING_AI_OPENAI_API_KEY=<YOUR OPENAI API KEY>
+```
---
+++ src/main/java/dev/bogdanjovanovic/leafchatbot/LeafChatbotApplication.java
@@ -0,0 +1,13 @@
+package dev.bogdanjovanovic.leafchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafChatbotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LeafChatbotApplication.class, args);
+  }
+
+}
