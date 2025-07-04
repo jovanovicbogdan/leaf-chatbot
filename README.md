383b8a4a72187333362a3827cfd886c1afec7141
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2025-07-04 18:15:06 +0200

    feat: write pom configuration for building container image

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 # Leaf Chatbot
 
+Leaf Chatbot is a Retrieval-Augmented Generation (RAG) application designed to answer questions based on the content of ingested Markdown documents. LEAF (Lean Enterprise Automation Framework) is a test automation framework built on top of Playwright, used for E2E, UI and API testing.
+
+## Tech stack
+
+- Java 17
+- Maven
+- Spring Boot
+- Spring AI
+- PGVector
+- Docker
+
+## How to run
+
+1. Set `OPENAI_API_KEY` environment variable.
+2. Put Markdown files in `leaf-docs` directory in root project dir.
+3. Run `docker compose up -d`
+
+Ingesting data will calculate the embeddings of provided markdown files using OpenAI embedding model `text-embedding-ada-002` and store them in the `pgvector` database. Note that the endpoint below will first delete embeddings if there are any and recalculate again.
+
 ```shell
-export OPENAI_API_KEY=<YOUR OPENAI API KEY>
+POST http://localhost:8080/api/v1/ingest
+```
+
+Ask a question
+
+```shell
+POST http://localhost:0880/api/v1/conversation
+
+{
+  "question": "How to create Page Object Model using LEAF?"
+}
+```
+
+## Build an image and publish to a private registry
+
+```shell
+mvn spring-boot:build-image \
+  -Dimage.publish=true \
+  -Ddocker.publishRegistry.username=$DOCKER_USERNAME \
+  -Ddocker.publishRegistry.password=$DOCKER_PAT
 ```
--- src/main/java/dev/bogdanjovanovic/leafchatbot/LeafChatbotApplication.java
+++ src/main/java/dev/bogdanjovanovic/leafchatbot/LeafChatbotApplication.java
@@ -1,56 +1,13 @@
 package dev.bogdanjovanovic.leafchatbot;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.ai.vectorstore.SearchRequest;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LeafChatbotApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LeafChatbotApplication.class, args);
-  }
-
-  @Bean
-  CommandLineRunner commandLineRunner(
-      @Qualifier("openAiChatClient") ChatClient openAiChatClient,
-      LeafMarkdownReader markdownReader,
-      VectorStore vectorStore,
-      OpenAiChatModel openAiChatModel) {
-    return args -> {
-//      ChatResponse chatResponse = openAiChatClient.prompt()
-//          .user("Tell me a joke")
-//          .call()
-//          .chatResponse();
-//      System.out.println(chatResponse.getResult().getOutput().getText());
-      final var documents = markdownReader.loadMarkdown();
-
-//      documents.forEach(d -> {
-//        vectorStore.add(d.splitDocuments());
-//      });
-
-//      final var results = vectorStore.similaritySearch(SearchRequest.builder()
-//          .query("POMs")
-//          .topK(5)
-//          .build());
-
-      final var chatResponse = ChatClient.builder(openAiChatModel)
-          .build().prompt()
-          .advisors(new QuestionAnswerAdvisor(vectorStore))
-          .user("What is POM container and how to use it?")
-          .call()
-          .chatResponse();
-
-      System.out.println(chatResponse.getResult().getOutput().getText());
-    };
   }
 
 }
